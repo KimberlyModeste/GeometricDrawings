f5c47b3c1fe13892eb16a772a360f638b2760656
Kimberly Modeste <KimberlJModeste@gmail.com>
2021-10-20 14:54:34 -0400

    Final

--- HW2-P3/main.cpp
+++ HW2-P3/main.cpp
@@ -12,44 +12,40 @@ Canvas cvs(640, 640, title);
 Point2 outer[7];
 Point2 inner[7];
 bool isOut;
-bool clearOutline;
 
-GLint p = 1;
-int clicker = 4;
+int clicker = 1;
 bool hasClicked = false;
-float currentColor[] = { 0.0,0.0,0.0 };
 
 void ngon(int n, float cx, float cy, float radius, float rotAngle) {
 	if (n < 3) return;
 	double angle = rotAngle * 3.14159265 / 180;
 	double angleInc = 2 * 3.14159265 / n;
 	cvs.moveTo(radius * cos(angle) + cx, radius * sin(angle) + cy);
-	if (clearOutline)
-		cvs.setColor(1.0, 1.0, 1.0);
 
 	for (int k = 0; k <= n; k++)
 	{
-		cvs.lineTo(radius * cos(angle) + cx, radius * sin(angle) + cy);
-		angle += angleInc;
-
 		if (isOut)
+		{
 			outer[k].set(radius * cos(angle) + cx, radius * sin(angle) + cy);
+			cvs.lineTo(radius * cos(angle) + cx, radius * sin(angle) + cy);
+		}
+			
 		else if (!isOut)
 			inner[k].set(radius * cos(angle) + cx, radius * sin(angle) + cy);
+
+		angle += angleInc;
 	}
 }
-
 
 void drawA(int x, int y) {
 	
 	isOut = true;
-	clearOutline = false;
+	
 	ngon(3, x, y, 50.0, 90.0);
 
 	isOut = false;
-	clearOutline = true;
+
 	ngon(3, x, y, 5.0, 30.0);
-	cvs.setColorfv(currentColor);
 	cvs.moveTo(outer[0]);
 	cvs.lineTo(inner[0]);
 	cvs.lineTo(outer[2]);
@@ -62,13 +58,13 @@ void drawA(int x, int y) {
 void drawB(int x, int y) {
 
 	isOut = true;
-	clearOutline = false;
+	
 	ngon(5, x, y, 50.0, 18.0);
 
 	isOut = false;
-	clearOutline = true;
+
 	ngon(5, x, y, 5.0, 53.0);
-	cvs.setColorfv(currentColor);
+
 	cvs.moveTo(outer[0]);
 	cvs.lineTo(inner[4]);
 	cvs.lineTo(outer[4]);
@@ -84,14 +80,14 @@ void drawB(int x, int y) {
 }
 
 void drawC(int x, int y) {
-	clearOutline = true;
+
 	
 	isOut = true;
 	ngon(7, x, y, 50.0, 38.5);
 	isOut = false;
 	ngon(7, x, y, 10.0, 13.0);
 
-	cvs.setColorfv(currentColor);
+
 	cvs.moveTo(outer[0]);
 	cvs.lineTo(inner[1]);
 	cvs.lineTo(outer[1]);
@@ -114,16 +110,12 @@ void drawC(int x, int y) {
 void drawD(int x, int y) {
 	
 	isOut = true;
-	clearOutline = false;
 	ngon(6, x, y, 50.0, 90.0);
-	isOut = NULL;
-	clearOutline = false;
-	ngon(60, x, y, 15.0, 0.0);
+
 	isOut = false;
-	clearOutline = true;
 	ngon(6, x, y, 15.0, 0.0);
 
-	cvs.setColorfv(currentColor);
+
 	cvs.moveTo(outer[0]);
 	cvs.lineTo(inner[1]);
 	cvs.lineTo(outer[5]);
@@ -138,10 +130,9 @@ void drawD(int x, int y) {
 	cvs.lineTo(inner[2]);
 	cvs.lineTo(outer[0]);
 
-	clearOutline = false;
-	ngon(50, x, y, 15.0, 0.0);
+	isOut = true;
+	ngon(60, x, y, 15.0, 0.0);
 	
-
 }
 
 void myMouse(int button, int state, int x, int y) {
@@ -159,104 +150,136 @@ void myMouse(int button, int state, int x, int y) {
 
 		else if (clicker == 4)
 			drawD(x, 640 - y);
-
-		hasClicked = true;
 	}
-
+	hasClicked = true;
 }
 
 void mainMenu(int id) {
-	if (id > 0 && id < 5)
 		clicker = id;
-
-	else if (id == 5)
-		exit(0);
-
-	cout << clicker << endl;
 }
 
 void colorsMenu(int id) {
 
 	//Red
 	if (id == 1)
-	{
 		cvs.setColor(1.0, 0.0, 0.0);
-		currentColor[0] = 1.0;  currentColor[1] = 0.0; currentColor[2] = 0.0;
-	}	
 
 	//Green
 	else if (id == 2)
-	{
 		cvs.setColor(0.0, 1.0, 0.0);
-		currentColor[0] = 0.0;  currentColor[1] = 1.0; currentColor[2] = 0.0;
-	}
 
 	//Blue
 	else if (id == 3)
-	{
 		cvs.setColor(0.0, 0.0, 1.0);
-		currentColor[0] = 0.0;  currentColor[1] = 0.0; currentColor[2] = 1.0;
-	}
 
 	//Cyan
 	else if (id == 4)
-	{
 		cvs.setColor(0.0, 1.0, 1.0);
-		currentColor[0] = 0.0;  currentColor[1] = 1.0; currentColor[2] = 1.0;
-	}
 
 	//Magenta
 	else if (id == 5)
-	{
 		cvs.setColor(1.0, 0.0, 1.0);
-		currentColor[0] = 1.0;  currentColor[1] = 0.0; currentColor[2] = 1.0;
-
-	}
 
 	//Yellow
 	else if (id == 6)
-	{
 		cvs.setColor(1.0, 1.0, 0.0);
-		currentColor[0] = 1.0;  currentColor[1] = 1.0; currentColor[2] = 0.0;
-
-	}
 
 	//Orange
 	else if (id == 7)
-	{
 		cvs.setColor(0.98, 0.63, 0.12);
-		currentColor[0] = 0.98;  currentColor[1] = 0.63; currentColor[2] = 0.12;
-
-	}
 
 	//Black
 	else if (id == 8)
-	{
 		cvs.setColor(0.0, 0.0, 0.0);
-		currentColor[0] = 0.0;  currentColor[1] = 0.0; currentColor[2] = 0.0;
 
+	//White
+	else if (id == 9) 
+		cvs.setColor(1.0, 1.0, 1.0);
+
+
+
+	//Red Background
+	if (id == 10)
+	{
+		cvs.setBackgroundColor(1.0, 0.0, 0.0);
+		cvs.clearScreen();
+	}
+
+	//Green Background
+	else if (id == 11)
+	{
+		cvs.setBackgroundColor(0.0, 1.0, 0.0);
+		cvs.clearScreen();
+	}
+
+	//Blue Background
+	else if (id == 12)
+	{
+		cvs.setBackgroundColor(0.0, 0.0, 1.0);
+		cvs.clearScreen();
+	}
+
+	//Cyan Background
+	else if (id == 13)
+	{
+		cvs.setBackgroundColor(0.0, 1.0, 1.0);
+		cvs.clearScreen();
+	}
+	
+	//Magenta Background
+	else if (id == 14)
+	{
+		cvs.setBackgroundColor(1.0, 0.0, 1.0);
+		cvs.clearScreen();
+	}
+	
+	//Yellow Background
+	else if (id == 15)
+	{
+		glClearColor(1.0, 1.0, 0.0, 0.0);
+		cvs.clearScreen();
+	}
+	
+	//Orange Background
+	else if (id == 16)
+	{
+		cvs.setBackgroundColor(0.98, 0.63, 0.12);
+		cvs.clearScreen();
+	}
+	
+	//Black Background
+	else if (id == 17)
+	{
+		cvs.setBackgroundColor(0.0, 0.0, 0.0);
+		cvs.clearScreen();
+	}
+	
+	//White Background
+	else if (id == 18)
+	{
+		cvs.setBackgroundColor(1.0, 1.0, 1.0);
+		cvs.clearScreen();
 	}
 }
 
-void pixelMenu(int id) {
+void backgroundMenu(int id) {
 	if (id == 1)
-		p = p * 2;
+		cvs.clearScreen();
 
-	else if (id == 2)
-		if (p > 1)
-			p = p * 0.5;
-
-	glPointSize(p);
+	 else if (id == 2)
+		 exit(0);
 }
 
 void display() {
 
+	//Do this only once
 	if (!hasClicked)
 	{
 		cvs.clearScreen();
 		cvs.setColor(0.0, 0.0, 0.0);
-	}		
-	cvs.setBackgroundColor(1.0, 1.0, 1.0);
+		cvs.setBackgroundColor(1.0, 1.0, 1.0);	
+	}
+
 	cvs.setWindow(0.0, 640.0, 0.0, 640.0);
 	cvs.setViewport(0, 640, 0, 640);
 
@@ -268,9 +291,10 @@ void display() {
 
 void main(int argc, char** argv)
 {
-	//color for lines and line thickness
-	int c_menu, p_menu;
+	//colors and background
+	int c_menu, bc_menu, b_menu;
 
+	//Line colors
 	c_menu = glutCreateMenu(colorsMenu);
 	glutAddMenuEntry("Red", 1);
 	glutAddMenuEntry("Green", 2);
@@ -280,20 +304,35 @@ void main(int argc, char** argv)
 	glutAddMenuEntry("Yellow", 6);
 	glutAddMenuEntry("Orange", 7);
 	glutAddMenuEntry("Black", 8);
+	glutAddMenuEntry("White", 9);
 
-	p_menu = glutCreateMenu(pixelMenu);
-	glutAddMenuEntry("Increase Pixel Size", 1);
-	glutAddMenuEntry("Decrease Pixel Size", 2);
+	//Background colors
+	bc_menu = glutCreateMenu(colorsMenu);
+	glutAddMenuEntry("Red", 10);
+	glutAddMenuEntry("Green", 11);
+	glutAddMenuEntry("Blue", 12);
+	glutAddMenuEntry("Cyan", 13);
+	glutAddMenuEntry("Magenta", 14);
+	glutAddMenuEntry("Yellow", 15);
+	glutAddMenuEntry("Orange", 16);
+	glutAddMenuEntry("Black", 17);
+	glutAddMenuEntry("White", 18);
 
 
+	//Background 
+	b_menu = glutCreateMenu(backgroundMenu);
+	glutAddSubMenu("Background Colors", bc_menu);
+	glutAddMenuEntry("Clear Screen", 1);
+	glutAddMenuEntry("Quit", 2);
+	
+	//MAin menu
 	glutCreateMenu(mainMenu);
 	glutAddSubMenu("Colors", c_menu);
-	glutAddSubMenu("Pixel Size", p_menu);
+	glutAddSubMenu("Background Menu", b_menu);
 	glutAddMenuEntry("Shape A", 1);
 	glutAddMenuEntry("Shape B", 2);
 	glutAddMenuEntry("Shape C", 3);
 	glutAddMenuEntry("Shape D", 4);
-	glutAddMenuEntry("Quit", 5);
 
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
